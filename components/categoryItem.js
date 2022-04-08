179729a21a7099596acfeb3238c81f36d721d3c7
Msdalmajdi <msdalmajdi@hotmail.com>
2022-04-08 09:13:35 +0300

    MODIFIED with finishing all javascript requests, but not edge cases and stuff, now getting ready to implement Bootstrap

--- components/categoryItem.js
+++ components/categoryItem.js
@@ -2,7 +2,13 @@ function categoryItemComponent(categoryName) {
   // You are going to add your HTML design here.
   // Do the option design in HTML page, test it, and replace it if you need instead of this code.
   // For any variiables, just use the interpolation operation ${} and pass the variable
-  return `
+  if (categoryName === "Select Continent") {
+    return `
+            <option value="${categoryName}" class="category-item"  disabled selected>${categoryName}</option>
+    `;
+  } else {
+    return `
             <option value="${categoryName}" class="category-item">${categoryName}</option>
     `;
+  }
 }
--- index.js
+++ index.js
@@ -2,38 +2,45 @@
 const CATEGORY_SELECTOR = "categories-list";
 const CATEGORY_FILTER = "categories-list-filter";
 
-let tasks = [];
-let categories = [];
+let tasks = []; // array of objects according to sample ==>
+let categories = ["Select Continent"]; // array of strings according to sample ==>
 
-// REMOVE ME: SAMPLE FILLING
-tasks = [
-  { id: 0, title: "Game of thrones", category: "Movies", done: false },
-  { id: 1, title: "Toy Story 4", category: "Movies", done: false },
-];
-
-categories = ["Movies", "Groceries"];
-// SAMPLE
 renderCategories(categories, CATEGORY_SELECTOR);
 renderCategories(categories, CATEGORY_FILTER);
 renderTasks(tasks, "tasks-list");
 
 function taskChecked(taskId, checked) {
-  // implement the delete task.
-  // You are given the task id
-  console.log(`${checked ? "" : "UN"}CHECKED TASK`, taskId);
+  //continue the code here
+  tasks[taskId]["done"] = checked;
+  alert(`${checked ? "" : "UN"}CHECKED TASK`, taskId);
 }
 
 function addTask() {
-  const selectedCategory = getSelectedCategoryById(CATEGORY_SELECTOR);
+  var selectedCategory = getSelectedCategoryById(CATEGORY_SELECTOR);
   const taskTitle = getNewTaskText();
   // continue the code here
+  if (selectedCategory === "Select Continent") {
+    selectedCategory = "Uncategorized";
+    alert("Choose a category!");
+    return;
+  }
+  tasks.push({
+    id: tasks.length,
+    title: taskTitle,
+    category: selectedCategory,
+    done: false,
+  });
   alert(`Category: ${selectedCategory} | Task: ${taskTitle}`);
+  renderTasks(tasks, "tasks-list");
 }
 
 function addCategory() {
   const newCategory = getNewCategoryText();
   // continue the code here
+  categories.push(newCategory);
   alert(`New category was added: ${newCategory}`);
+  renderCategories(categories, CATEGORY_SELECTOR);
+  renderCategories(categories, CATEGORY_FILTER);
 }
 
 function filterTasks() {
@@ -41,5 +48,29 @@ function filterTasks() {
   const done = getFilteredDone();
   // continue the code here
   // REMOVE ME: sample alert
+  if (done) {
+    var filteredTasks = tasks.filter(
+      (task) => task["category"] === selectedCategory && task["done"] === true
+    );
+  } else {
+    var filteredTasks = tasks.filter(
+      (task) => task["category"] === selectedCategory
+    );
+  }
   alert(`Category: ${selectedCategory} | done: ${done}`);
+  renderTasks(filteredTasks, "tasks-list");
 }
+
+// my addition
+
+// REMOVE ME: SAMPLE FILLING
+/*
+tasks = [
+  { id: 0, title: "Game of thrones", category: "Movies", done: false },
+  { id: 1, title: "Toy Story 4", category: "Movies", done: false },
+];
+
+categories = ["Movies", "Groceries"];
+// SAMPLE
+
+*/
